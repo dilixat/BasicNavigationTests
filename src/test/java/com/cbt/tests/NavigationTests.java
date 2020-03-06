b226a942cdc5ffa1e971b4fb6e9003efb3ae2d9c
dilixat928@gmail.com <dilixat928@gmail.com>
2020-03-06 09:02:51 -0800

    project

--- src/test/java/com/cbt/tests/NavigationTests.java
+++ src/test/java/com/cbt/tests/NavigationTests.java
@@ -3,7 +3,20 @@ package com.cbt.tests;
 import com.cbt.utilities.BrowserFactory;
 import org.openqa.selenium.WebDriver;
 
+import static com.cbt.utilities.StringUtility.verifyEquals;
+
 public class NavigationTests {
+//  2. Go to website https://google.com.
+//  3. Save the title in a string variable
+//  4. Go to https://etsy.com
+//  5. Save the title in a string variable
+//  6. Navigate back to previous page
+//  7. Verify that title is same is in step 3
+//  8. Navigate forward to previous page
+//  9. Verify that title is same is in step 5
+
+
+
     public static void main(String[] args) {
 
        // edge();
@@ -13,12 +26,25 @@ public class NavigationTests {
     public static void chromeTest() {
         WebDriver driver = BrowserFactory.getDriver("chrome");
         driver.get("https://google.com");
+          String saveTitle=driver.getTitle();
+          driver.get(" https://etsy.com");
+          String saveTitle2=driver.getTitle();
+          driver.navigate().back();
+
+          verifyEquals(saveTitle,saveTitle2);
+
+          driver.navigate().forward();
+
+          verifyEquals(saveTitle,saveTitle2);
+
+
+
+
+
+
     }
 
-    public static void edge() {
-        WebDriver driver = BrowserFactory.getDriver("Edge");
-        driver.get("https://google.com");
-    }
+
 }
 
 
